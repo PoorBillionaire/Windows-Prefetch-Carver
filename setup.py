a779f5330dab73f4c6ec6408cfaa5792c7ef3152
PoorBillionaire <adam.witt@yahoo.com>
2017-05-26 08:51:30 -0700

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='prefetchcarve',
+    version='1.0.0',
+    description='A Python script to carve Windows Prefetch artifacts from arbitrary binary data',
+    long_description=long_description,
+    url='https://github.com/PoorBillionaire/Windows-Prefetch-Carver',
+    author='Adam Witt',
+    author_email='accidentalassist@gmail.com',
+    license='Apache Software License',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Topic :: Security',
+        'License :: OSI Approved :: Apache Software License'
+    ],
+
+    packages=find_packages(),
+    scripts=['prefetch-carve.py']
+)
